67a85a677e95bcbf5e11910d743ffe5fff7d94c8
Subhrajyoti2608 <86191223+Subhrajyoti2608@users.noreply.github.com>
2022-07-04 18:49:36 +0530

    Add files via upload

---
+++ move_File.py
@@ -0,0 +1,8 @@
+import os
+
+
+source = "C:/Users/subhr/Downloads/NewImage.jpg"
+dest = "C:/Users/subhr/Downloads/TheGoldenFortess.jpg"
+
+os.rename(source,dest)
+print("Your file renamed successfully!")
